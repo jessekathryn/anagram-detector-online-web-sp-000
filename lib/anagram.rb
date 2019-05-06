a989eea197e6b36b814f107874d5c7fb283be423
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-06 00:24:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class EmailParser
+  
+attr_accessor :emails
+
+def initialize(emails)
+ @emails = emails
+end
+
+emails = []
+
+def parse
+ @emails = emails.split(/, | /)
+ @emails.uniq
+ end
+end
+
