a61ed82995868a94f3c9a65e8b3eff4cdb22c436
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-06 01:37:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,12 @@ class Anagram
 attr_accessor :words
 
 def initialize(words)
- @words = words.split("").collect{|l| l.sort}
+ @words = words
 end
 
 def match(words)
  matching_letters = []
+ @words = words.split("").collect{|l| l.sort}
  @words.each do |word|
   if @words == word
    @words
